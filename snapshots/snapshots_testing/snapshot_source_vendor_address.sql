e13ce55e8910f5e0ffc7191b8b598077cf502a3a
Hope Watson <hope.watson@fishtownanalytics.com>
2024-10-18 18:16:53 +0000

    add snapshot experimentation code

---
+++ snapshots/snapshots_testing/snapshot_source_vendor_address.sql
@@ -0,0 +1,1 @@
+select * from {{ source('daimler_mock', 'vendor_address') }}
---
+++ snapshots/snapshots_testing/stg_vendor_surrogate_key_null_testing.sql
@@ -0,0 +1,4 @@
+ select {{ dbt_utils.generate_surrogate_key(['VNDR_ID', 'ADDR_OWNER', 'ADDR_FUNCT_DP', 'ADDR_LN_1', 'CITY', 'ST_CD', 'ZIP_CD', 'CTRY_CD'
+]) }},
+*
+ from {{ source('daimler_mock', 'vendor_address') }}
