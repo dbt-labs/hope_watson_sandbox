22c430e69184ea086857d2188f36434f60b6bac4
Hope Watson <hope.watson@fishtownanalytics.com>
2024-10-30 14:43:04 +0000

    remove retrofit and update the tpch_orders snapshot

--- snapshots/snapshot_tpch_orders.sql
+++ snapshots/snapshot_tpch_orders.sql
@@ -2,10 +2,20 @@
 
 {{
     config(
-      unique_key='o_orderkey',
+      unique_key = ['o_orderkey', 'o_orderdate'],
       schema='snapshots', 
-      strategy='timestamp',
-      updated_at='o_orderdate', 
+      strategy='check',
+      check_cols=[
+            'o_orderkey',
+            'o_custkey',
+            'o_orderstatus',
+            'o_totalprice',
+            'o_orderdate',
+            'o_orderpriority',
+            'o_clerk',
+            'o_shippriority',
+            'o_comment'
+        ],
       snapshot_meta_column_names={
               'dbt_valid_from': 'valid_from',
               'dbt_valid_to': 'valid_to'
