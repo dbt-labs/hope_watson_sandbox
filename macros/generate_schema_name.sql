314169ad97430ea993e73f8b57c5c0c8753f8e1e
Hope Watson <hope.watson@fishtownanalytics.com>
2025-02-04 17:25:46 +0000

    keep verbose logging for now

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,13 +1,9 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- set env = env_var('DBT_CURRENT_ENV', '') -%}
     
-    {# Ensure logging happens only once per run #}
-{% if execute and not context.get('schema_log_shown', False) %}
     {% do log("dbt Run Environment: " ~ env_var('DBT_CURRENT_ENV', ''), info=True) %}
     {% do log("dbt Target Schema: " ~ target.schema, info=True) %}
     {% do context.update({'schema_log_shown': True}) %}
-{% endif %}
-
 
     {%- if env == 'dev' -%}
         {{ target.schema }}
