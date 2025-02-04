ac7d7e6acbc9dd237c9e7e28f5e17f0cc6f0b48f
Hope Watson <hope.watson@fishtownanalytics.com>
2025-02-04 16:12:24 +0000

    I want to make sure I see the schema in logs, trying this

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,16 +1,19 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- set env = env_var('DBT_CURRENT_ENV', '') -%}
-    
+
     {% do log("Target Schema: " ~ target.schema, info=True) %}
     {% do log("Custom Schema Name: " ~ (custom_schema_name if custom_schema_name else 'None'), info=True) %}
     {% do log("Environment: " ~ env, info=True) %}
 
+
     {%- if env == 'dev' -%}
         {{ target.schema }}
     {%- elif env == 'prod' -%}
-        {{ custom_schema_name | default(target.schema) | trim }}
+        {# Ensure a valid schema is always assigned #}
+        {{ (custom_schema_name | trim) if custom_schema_name else target.schema }}
     {%- else -%}
         {% do exceptions.raise_compiler_error("Invalid DBT_CURRENT_ENV. Must be 'dev' or 'prod'.") %}
     {%- endif -%}
 
 {%- endmacro %}
+
